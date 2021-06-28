816481015169e5313b4d877e5b15dd1390cb6f8a
Atya Saeed <58461928+atyasaeed@users.noreply.github.com>
2021-06-28 12:47:50 +0200

    todo class

--- todo/src/main/java/todo/todo/controller/TodoController.java
+++ todo/src/main/java/todo/todo/controller/TodoController.java
@@ -1,9 +1,38 @@
 package todo.todo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import todo.todo.model.Todo;
+import todo.todo.service.TodoService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping(name = "/finAll")
+    public List<Todo> findAll() {
+
+        return todoService.findAll();
+
+    }
+
+    @GetMapping(name = "/findBy/{id}")
+    public Todo finById(@PathVariable("id") Integer id) {
+        return todoService.findById(id);
+    }
+
+    @PostMapping(name = "/save")
+    public Todo save(@RequestBody Todo todo) {
+        return todoService.save(todo);
+    }
+
+    @DeleteMapping(name = "/delete/{id}")
+    public boolean deleteById(@PathVariable("id") Integer id) {
+        return todoService.deleteById(id);
+    }
+
 }
---
+++ todo/src/main/java/todo/todo/service/TodoService.java
@@ -0,0 +1,59 @@
+package todo.todo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import todo.todo.model.Todo;
+import todo.todo.repository.TodoRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> findAll() {
+        try {
+            return todoRepository.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public Todo save(Todo todo) {
+        try {
+            return todoRepository.save(todo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public boolean deleteById(Integer id) {
+        try {
+            todoRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public Todo findById(Integer id) {
+        try {
+            Optional<Todo> byId = todoRepository.findById(id);
+            if (byId.isPresent()) {
+                return byId.get();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
